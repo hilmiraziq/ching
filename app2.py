7e3639763ec085c462749282601f11957e36a9d4
hilmiraziq <136722242+hilmiraziq@users.noreply.github.com>
2023-07-06 14:10:08 +0700

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
